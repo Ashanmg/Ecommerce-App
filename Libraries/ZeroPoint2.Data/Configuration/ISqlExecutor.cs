3abe2dd0fe34c00de79ab0c989e7a9d020a0e4a5
Kevin Fernando <ashanmg9@gmail.com>
2021-10-22 13:25:18 +0530

    core layer changes

---
+++ Libraries/ZeroPoint2.Data/Configuration/ISqlExecutor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace ZeroPoint2.Data.Configuration
+{
+    public interface ISqlExecutor
+    {
+        Task ExecuteSQLAsync(Func<IDbConnection, Task> action, IDbCommand command = null, object data = null);
+
+        bool IsRetryable(Exception exceptionTrown);
+    }
+}
---
+++ Libraries/ZeroPoint2.Data/Configuration/SqlExecutor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ZeroPoint2.Core;
+
+namespace ZeroPoint2.Data.Configuration
+{
+    public class SqlExecutor : ISqlExecutor
+    {
+        private readonly string connectionString;
+
+        public SqlExecutor(IConfiguration config)
+        {
+            connectionString = config.GetValue<string>("ConnectionConfig:SQLConnection:ConnectionString");
+        }
+
+        public async Task ExecuteSQLAsync(Func<IDbConnection, Task> action, IDbCommand command = null, object data = null)
+        {
+            await ActionRetryer.RetryWhenExceptionAsync(async () =>
+            {
+                using (var con = new SqlConnection { ConnectionString = connectionString })
+                {
+                    con.Open();
+
+                    await action(con);
+                }
+            },
+               IsRetryable
+            );
+        }
+
+        public bool IsRetryable(Exception exceptionTrown)
+        {
+            return
+                (
+                    new List<Type>
+                    {
+                        typeof(SqlException)
+                    }
+                )
+                .Any(e => e == exceptionTrown.GetType());
+        }
+    }
+}
