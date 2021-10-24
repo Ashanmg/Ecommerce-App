b2b6b05fafde94384654dfc478f0059023ceb7db
Kevin Fernando <ashanmg9@gmail.com>
2021-10-24 13:04:10 +0530

    text changes and signup page back ground changes.

--- Presentation/ZeroPoint2/ClientApp/src/screens/SignUpScreen/SignUpScreen.js
+++ Presentation/ZeroPoint2/ClientApp/src/screens/SignUpScreen/SignUpScreen.js
@@ -31,7 +31,7 @@ export const SignUpScreen = ({ className, ...restProps }) => {
       style={{ backgroundColor: '#01a45300' }}
     >
       <div className="sign-up-screen__wrapper flex justify-center items-center w-1/2 h-3/4">
-        <div className="sign-up-screen__left flex-1 bg-white p-10 border-G-100 border-4 h-full flex flex-col justify-center">
+        <div className="sign-up-screen__left flex-1 bg-white p-10 border-G-light border-4 h-full flex flex-col justify-center">
           <div className="sign-up-screen__left__title text-G-dark text-2xl font-semibold text-center mb-8">
             Create Account
           </div>
@@ -57,7 +57,7 @@ export const SignUpScreen = ({ className, ...restProps }) => {
             </form>
           </div>
         </div>
-        <div className="sign-up-screen__right flex-1 flex flex-col items-center justify-center h-full bg-G-100 p-10">
+        <div className="sign-up-screen__right flex-1 flex flex-col items-center justify-center h-full bg-G-light p-10">
           <div className="logo flex-initial flex flex-col items-center mb-8">
             <Link to="/" className="flex justify-center">
               <img
@@ -71,7 +71,7 @@ export const SignUpScreen = ({ className, ...restProps }) => {
             </div>
           </div>
           <div className="flex flex-row h-7 md:h-10 items-center text-center italic text-G-dark text-md md:text-base xl:text-lg font-medium mb-8">
-            Your one-stop shop for giving -20% of every sale Donated to the
+            Your one-stop shop for giving - 20% of every sale donated to the
             charity of your choice.&nbsp;{' '}
             <RiInformationLine className="hidden lg:flex" />
           </div>
