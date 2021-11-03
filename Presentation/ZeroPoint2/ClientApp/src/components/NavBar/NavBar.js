bf5324775478addd7f8fbbaf5aa7ecff0088956f
isuru <sampathisuru516@gmail.com>
2021-11-04 02:37:13 +0530

    mobile reponsive nav-bar

--- Presentation/ZeroPoint2/ClientApp/src/components/NavBar/NavBar.js
+++ Presentation/ZeroPoint2/ClientApp/src/components/NavBar/NavBar.js
@@ -8,6 +8,8 @@ import TextField from '../TextField/TextField';
 import Button from '../Button/Button';
 
 import zeroLogo from '../../assets/zeroLogo.png';
+import useMediaQuery from '../../config/customHooks/useMediaQuery';
+
 import './NavBar.scss';
 
 export const NavBar = ({ className, handleToggle, ...restProps }) => {
@@ -18,6 +20,78 @@ export const NavBar = ({ className, handleToggle, ...restProps }) => {
   );
 
   const [passwordShow, setPasswordShow] = useState(false);
+
+  const isSmallWide = useMediaQuery('(max-width: 640px)');
+
+  if (isSmallWide) {
+    return (
+      <motion.div
+        className={CN(NavBarClasses, 'px-3')}
+        {...restProps}
+        initial={{ y: -250 }}
+        animate={{ y: -10 }}
+        transition={{ stiffness: 120 }}
+      >
+        <div className="flex flex-col items-center pt-5">
+          <div className="mr-1 logo lg:mr-3">
+            <Link to="/" className="flex justify-center">
+              <img src={zeroLogo} alt="Logo" className="object-fill w-1/2" />
+            </Link>
+          </div>
+          <div className="text-xs italic logo_text text-G-dark">
+            Environmental Socially gifts
+          </div>
+        </div>
+        <div className="flex pb-2 mt-2 text-base italic nav-bar-buttom text-G-dark">
+          your one-shop shop for giving -20% of every sale donated to the
+          charity fo your choice
+        </div>
+        <div>
+          <form action="" method="post">
+            <div className="flex justify-around flex-grow mb-2">
+              <div className="flex-1 mr-1 email-field">
+                <TextField placeholder="Email" />
+              </div>
+              <div className="flex-1 password-field">
+                <TextField
+                  placeholder="Password"
+                  onClickIconAfter={() => {
+                    setPasswordShow(!passwordShow);
+                  }}
+                  type={passwordShow ? 'text' : 'password'}
+                  iconAfter={
+                    passwordShow ? (
+                      <RiEyeOffLine size={20} />
+                    ) : (
+                      <RiEyeLine size={20} />
+                    )
+                  }
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-center ml-9">
+              <div className="mr-1 signIn-btn lg:mr-3">
+                <Button
+                  children="Sign In"
+                  className="items-center px-5 py-1 text-xs text-white border-2 h-7 w-max md:h-8 lg:h-10 md:py-2 bg-G-light lg:text-sm border-G-light hover:bg-white hover:text-G-dark"
+                />
+              </div>
+              <div className="signUp-btn">
+                <Button
+                  children="Sign Up"
+                  className="items-center px-5 py-1 text-xs text-white border-2 h-7 w-max md:h-8 lg:h-10 md:py-2 bg-G-light lg:text-sm border-G-light hover:bg-white hover:text-G-dark"
+                  onClick={handleToggle}
+                />
+              </div>
+              <div className='ml-1'>
+                <RiInformationLine size={26} className="text-G-dark" />
+              </div>
+            </div>
+          </form>
+        </div>
+      </motion.div>
+    );
+  }
 
   return (
     <motion.div
