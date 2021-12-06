90e2ce5a2a82acd9ceb5348c3b3834a257051f70
Kevin Fernando <ashanmg9@gmail.com>
2021-12-07 00:03:50 +0530

    Add some margin to footer section

--- Presentation/ZeroPoint2/ClientApp/src/App.js
+++ Presentation/ZeroPoint2/ClientApp/src/App.js
@@ -98,7 +98,7 @@ function App() {
             ></Route>
           </Routes>
         </main>
-        <footer className="flex">
+        <footer className="flex mt-4">
           <Footer />
         </footer>
       </div>
