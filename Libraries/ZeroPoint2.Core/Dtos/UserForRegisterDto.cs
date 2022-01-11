ab7a529b2fb3e96ebe3e33c37579a60b3e88f0c3
Kevin Fernando <ashanmg9@gmail.com>
2022-01-11 18:38:50 +0530

    did some modification to appsettings.

--- Libraries/ZeroPoint2.Core/Dtos/UserForRegisterDto.cs
+++ Libraries/ZeroPoint2.Core/Dtos/UserForRegisterDto.cs
@@ -9,7 +9,7 @@ namespace ZeroPoint2.Core.Dtos
         [Required]
         public string Email { get; set; }
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters.")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 20 characters.")]
         public string Password { get; set; }
     }
 }
