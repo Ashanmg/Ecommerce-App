c36a62e5e161dc42c5e2a6a102b65e8aedf67e6c
Kevin Fernando <ashanmg9@gmail.com>
2022-03-27 14:29:29 +0530

    add dtos

---
+++ Libraries/ZeroPoint2.Core/Dtos/ProductForDetailDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroPoint2.Core.Dtos
+{
+    public class ProductForDetailDto
+    {
+        public string ProductName { get; set; }
+        public string ProductShortDescription { get; set; }
+        public string ProductLongDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public List<string> ProductImages { get; set; }
+        public decimal ProductQuantity { get; set; }
+        public List<string> ProductSizes { get; set; }
+        public List<ColorDto> ProductColors { get; set; }
+        public ShippingDetailDto ShippingDetails { get; set; }
+    }
+
+    public class ShippingDetailDto
+    {
+        public string ShippingType { get; set; }
+        public decimal ShippingCost { get; set; }
+        public string ShippingTime { get; set; }
+        public string ShippingDetails { get; set; }
+    }
+}
