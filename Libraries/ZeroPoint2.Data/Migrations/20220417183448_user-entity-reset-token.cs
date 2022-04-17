4276bf0fe76764cd43a91f1c3ba74f7af2a34756
Kevin Fernando <ashanmg9@gmail.com>
2022-04-18 00:05:48 +0530

    update database

---
+++ Libraries/ZeroPoint2.Data/Migrations/20220417183448_user-entity-reset-token.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZeroPoint2.Data.Migrations
+{
+    public partial class userentityresettoken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTokenUsed",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PasswordResetToken",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenCreatedOnUtc",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTokenUsed",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TokenCreatedOnUtc",
+                table: "Users");
+        }
+    }
+}
